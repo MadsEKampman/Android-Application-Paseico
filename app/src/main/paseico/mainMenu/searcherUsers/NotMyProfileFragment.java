6097657dbf1e7c65b730e7d784af0a15607bd77b
MadsEKampman <109226075+MadsEKampman@users.noreply.github.com>
2022-07-18 19:12:07 +0200

    Add files via upload

---
+++ app/src/main/paseico/mainMenu/searcherUsers/NotMyProfileFragment.java
@@ -0,0 +1,222 @@
+package app.paseico.mainMenu.searchUsers;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import app.paseico.R;
+import app.paseico.data.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.*;
+
+import java.util.List;
+
+public class NotMyProfileFragment extends Fragment {
+    ImageView image_profile;
+    TextView textView_followersNumber, textView_followersText, textView_followingText, textView_followingNumber, textView_fullname, textView_username;
+    FirebaseUser firebaseUser;
+    String profileid;
+    User actualUser;
+    User foreignUser;
+    String searchedUser;
+    Button buttonLogOut;
+    private Boolean firstTimeCheckBoost = false;
+    private User user = new User();
+    private String usernameFirebase;
+    private UserAdapter userAdapter;
+    private List<User> mUsers;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstaanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_notmyprofile, container, false);
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        SharedPreferences prefs = getContext().getSharedPreferences("PREFS", Context.MODE_PRIVATE);
+        profileid = prefs.getString("profileid", "none");
+        FirebaseDatabase.getInstance().getReference("users").child(firebaseUser.getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                actualUser = dataSnapshot.getValue(User.class);
+                FirebaseDatabase.getInstance().getReference("users").addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                            User user = snapshot.getValue(User.class);
+                            if (user.getUsername().equals(profileid)) {
+                                searchedUser = snapshot.getKey();
+                                FirebaseDatabase.getInstance().getReference("users").child(firebaseUser.getUid()).addValueEventListener(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                        User user = dataSnapshot.getValue(User.class);
+                                        usernameFirebase = user.getUsername();
+                                        userInfo();
+                                        getFollowers();
+                                        checkFollow();
+                                    }
+
+                                    @Override
+                                    public void onCancelled(@NonNull DatabaseError error) {
+
+                                    }
+                                });
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        image_profile = view.findViewById(R.id.image_profile);
+        textView_followersNumber = view.findViewById(R.id.textView_followersNumber);
+        textView_followingNumber = view.findViewById(R.id.textView_followingNumber);
+        textView_username = view.findViewById(R.id.username);
+        textView_fullname = view.findViewById(R.id.fullname);
+        buttonLogOut = view.findViewById(R.id.buttonLogOut);
+        textView_followersText = view.findViewById(R.id.textView_FollowersText);
+        textView_followingText = view.findViewById(R.id.textView_FollowingText);
+
+
+        buttonLogOut.setOnClickListener(v -> {
+            String btn = buttonLogOut.getText().toString();
+            if (btn.equals("Seguir")) {
+                FirebaseDatabase.getInstance().getReference().child("Follow").child(actualUser.getUsername())
+                        .child("following").child(profileid).setValue(true);
+                FirebaseDatabase.getInstance().getReference().child("Follow").child(profileid)
+                        .child("followers").child(actualUser.getUsername()).setValue(true);
+                buttonLogOut.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+
+            } else if (btn.equals("Siguiendo")) {
+                FirebaseDatabase.getInstance().getReference().child("Follow").child(actualUser.getUsername())
+                        .child("following").child(profileid).removeValue();
+                FirebaseDatabase.getInstance().getReference().child("Follow").child(profileid)
+                        .child("followers").child(actualUser.getUsername()).removeValue();
+                buttonLogOut.setBackground(getResources().getDrawable(R.drawable.gradient));
+            }
+        });
+
+
+        return view;
+    }
+
+    private void userInfo() {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("users").child(searchedUser);
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (getContext() == null) {
+                    return;
+                }
+                User user = snapshot.getValue(User.class);
+                foreignUser = user;
+                textView_username.setText(user.getUsername());
+                textView_fullname.setText(user.getName());
+                activateFollowListeners();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+    }
+
+    private void checkFollow() {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Follow").child(actualUser.getUsername()).child("following");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.child(profileid).exists()) {
+                    buttonLogOut.setText("Siguiendo");
+                    buttonLogOut.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                } else {
+                    buttonLogOut.setText("Seguir");
+                    buttonLogOut.setBackground(getResources().getDrawable(R.drawable.gradient));
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void getFollowers() {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Follow").child(profileid).child("followers");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                textView_followersNumber.setText("" + snapshot.getChildrenCount());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        DatabaseReference reference1 = FirebaseDatabase.getInstance().getReference().child("Follow").child(profileid).child("following");
+        reference1.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                textView_followingNumber.setText("" + snapshot.getChildrenCount());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    public void activateFollowListeners() {
+        textView_followersNumber.setOnClickListener(view -> {
+            Intent intent = new Intent(getContext(), FollowersActivity.class);
+            intent.putExtra("id", foreignUser.getUsername());
+            intent.putExtra("title", "followers");
+            startActivity(intent);
+        });
+
+        textView_followersText.setOnClickListener(view -> {
+            Intent intent = new Intent(getContext(), FollowersActivity.class);
+            intent.putExtra("id", foreignUser.getUsername());
+            intent.putExtra("title", "followers");
+            startActivity(intent);
+        });
+
+        textView_followingNumber.setOnClickListener(view -> {
+            Intent intent = new Intent(getContext(), FollowersActivity.class);
+            intent.putExtra("id", foreignUser.getUsername());
+            intent.putExtra("title", "following");
+            startActivity(intent);
+        });
+
+        textView_followingText.setOnClickListener(view -> {
+            Intent intent = new Intent(getContext(), FollowersActivity.class);
+            intent.putExtra("id", foreignUser.getUsername());
+            intent.putExtra("title", "following");
+            startActivity(intent);
+        });
+    }
+
+}
---
+++ app/src/main/paseico/mainMenu/searcherUsers/UserAdapter.java
@@ -0,0 +1,185 @@
+package app.paseico.mainMenu.searchUsers;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import app.paseico.R;
+import app.paseico.data.User;
+import app.paseico.mainMenu.profile.ProfileFragment;
+import app.paseico.service.FirebaseService;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.*;
+import de.hdodenhof.circleimageview.CircleImageView;
+
+import java.util.List;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
+
+    private Context context;
+    private List<User> users;
+    private boolean isFragment;
+    private FirebaseUser firebaseUser;
+    User actualUser;
+    private SearchUserFragment searchUserFragment;
+
+    public UserAdapter(Context context, List<User> users, boolean isFragment) {
+        this.context = context;
+        this.users = users;
+        this.isFragment = isFragment;
+    }
+
+    public UserAdapter(Context context, List<User> users, SearchUserFragment searchUserFragment) {
+        this.context = context;
+        this.users = users;
+        this.isFragment = true;
+        this.searchUserFragment = searchUserFragment;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.user_item, parent, false);
+        return new UserAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        final User searchedUser = users.get(position);
+        holder.btn_follow.setVisibility(View.VISIBLE);
+        holder.username.setText(searchedUser.getUsername());
+        holder.fullname.setText(searchedUser.getName());
+
+        FirebaseService.getCurrentRouterReference().addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                actualUser = dataSnapshot.getValue(User.class);
+                isFollowing(searchedUser.getUsername(), holder.btn_follow);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+
+        firebaseUser = FirebaseService.getCurrentUser();
+
+        holder.itemView.setOnClickListener(view -> {
+            if (isFragment) {
+                if (!isClickedUserTheCurrentUser(searchedUser)) {
+                    SharedPreferences.Editor editor = context.getSharedPreferences("PREFS", Context.MODE_PRIVATE).edit();
+                    editor.putString("profileid", searchedUser.getUsername());
+                    editor.apply();
+
+                    searchUserFragment.navigateToNotMyProfileFragment();
+                } else {
+                    searchUserFragment.navigateToProfileFragment();
+                }
+            } else {
+                    if (!isClickedUserTheCurrentUser(searchedUser)) {
+
+                        SharedPreferences.Editor editor = context.getSharedPreferences("PREFS", Context.MODE_PRIVATE).edit();
+                        editor.putString("profileid", searchedUser.getUsername());
+                        editor.apply();
+
+                        Fragment mFragment = new NotMyProfileFragment();
+
+                        ((FragmentActivity) context).getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.followerActivity_frameLayout, mFragment)
+                                .commit();
+                    }
+
+            }
+        });
+
+        holder.btn_follow.setOnClickListener(view -> {
+            if (holder.btn_follow.getText().toString().equals("Seguir")) {
+                FirebaseDatabase.getInstance().getReference().child("Follow").child(actualUser.getUsername())
+                        .child("following").child(searchedUser.getUsername()).setValue(true);
+                FirebaseDatabase.getInstance().getReference().child("Follow").child(searchedUser.getUsername())
+                        .child("followers").child(actualUser.getUsername()).setValue(true);
+                holder.btn_follow.setBackgroundColor(context.getResources().getColor(R.color.colorPrimary));
+            } else {
+                FirebaseDatabase.getInstance().getReference().child("Follow").child(actualUser.getUsername())
+                        .child("following").child(searchedUser.getUsername()).removeValue();
+                FirebaseDatabase.getInstance().getReference().child("Follow").child(searchedUser.getUsername())
+                        .child("followers").child(actualUser.getUsername()).removeValue();
+                holder.btn_follow.setBackground(context.getResources().getDrawable(R.drawable.gradient));
+            }
+        });
+    }
+
+    /**
+     * @return True if the User that has been clicked on is the same as the logged User, false if not.
+     */
+    private boolean isClickedUserTheCurrentUser(User searchedUser) {
+        return actualUser.getUsername().equals(searchedUser.getUsername());
+    }
+
+    @Override
+    public int getItemCount() {
+        return users.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView username;
+        public TextView fullname;
+        public CircleImageView image_profile;
+        public Button btn_follow;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            username = itemView.findViewById(R.id.username);
+            fullname = itemView.findViewById(R.id.fullname);
+            image_profile = itemView.findViewById(R.id.image_profile);
+            btn_follow = itemView.findViewById(R.id.btn_follow);
+        }
+    }
+
+    private void isFollowing(final String searchedUsername, Button button) {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Follow").child(actualUser.getUsername()).child("following");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (searchedUsername.equals(actualUser.getUsername())) {
+                    button.setVisibility(View.GONE);
+                } else {
+                    if (snapshot.child(searchedUsername).exists()) {
+                        button.setText("Siguiendo");
+                        button.setBackgroundColor(context.getResources().getColor(R.color.colorPrimary));
+                    } else {
+                        button.setText("Seguir");
+                        button.setBackground(context.getResources().getDrawable(R.drawable.gradient));
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+    }
+
+    private void getGetUsernameFromFirebase(String s) {
+        FirebaseDatabase.getInstance().getReference("users").child(firebaseUser.getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+                users.add(user);
+                notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+    }
+}
